6290089aa26b30257b2ee3a22b012e392b2063a4
KhawajaAsif <asif.majeed.pb@gmail.com>
2023-02-06 16:37:22 +0500

    commit20230206

---
+++ src/services/Basket/BASKET.API/Repositories/BaketRepository.cs
@@ -0,0 +1,32 @@
+﻿using BASKET.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace BASKET.API.Repositories
+{
+    public class BaketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BaketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public Task<ShoppingCart> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/services/Basket/BASKET.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using BASKET.API.Entities;
+
+namespace BASKET.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
